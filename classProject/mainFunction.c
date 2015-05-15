b01391097992c3a9bb88e263c89a5ca623dac96d
curogihu <curoas@yahoo.co.jp>
2015-05-14 18:18:56 -0700

    ソートの基準を追加
    登場件数が同じ場合、文字列の昇順で並べる。

--- classProject/mainFunction.c
+++ classProject/mainFunction.c
@@ -10,72 +10,109 @@
 #include <stdlib.h>		//atoi
 
 typedef struct{
-  char name[20];
+  char *name;
   int cnt;
 
 }NameData;
 
 int comp( const void *c1, const void *c2);
+int getRecordCntFromFile(FILE *targetFp);
+void closePointers(FILE *fp1, FILE *fp2);
 
 int main(){
 
 	FILE *inFp, *outFp;
-	NameData nameInfo[10];
-	int i;
-	int classNameLen = 0, dataCnt = 0;
 	char tmpIn[255] = {"\0"};
 	char tmpOut[255] = {"\0"};
-	char nameList[][20] = {"ClassName6508 7530",
-							"ClassName5660 4682",
-							"ClassName3500 444",
-							"ClassName6941 5317",
-							"ClassName1567 337",
-							"ClassName1677 5037",
-							"ClassName9929 4267",
-							"ClassName1969 3855",
-							"ClassName3631 3500",
-							"ClassName2976 5639"
-							};
-/*
-	if((inFp = fopen("sampleFile.txt", "r")) == NULL){
+	char tmpClassName[255] = {"\0"};
+	int i;
+	int dataCnt;
+
+	if((inFp = fopen("inputData2.txt", "r")) == NULL){
 		printf("Input file open error\n");
 		return 0;
 	}
-*/
 
-	if((outFp = fopen("output.txt", "w")) == NULL){
+	//if((outFp = fopen("./output.txt", "w")) == NULL){
+	if((outFp = fopen("outputData.txt", "w")) == NULL){
 		printf("Output file open error\n");
 		return 0;
 	}
 
-	for(i = 0; i< 10; i++){
-		// format is "[className] [repetitionCnt]", including blank.
-		sprintf(nameInfo[i].name, "%s", strtok(nameList[i], " "));
-		nameInfo[i].cnt = atoi(strtok(NULL, " "));
-/*
-		printf("%s:%d\n", nameInfo[i].name, nameInfo[i].cnt);
-*/
+	printf("Success, two files are opened.\n");
+
+	dataCnt = getRecordCntFromFile(inFp);
+
+	if(dataCnt <= 0){
+		printf("Program is finished because any data aren't found.\n");
+		closePointers(inFp, outFp);
 	}
 
-	qsort( nameInfo, 10, sizeof(NameData), comp );
-	printf("\n\n");
+	printf("Success, get records count.\n");
 
-	for(i = 0; i< 10; i++){
-/*
-		printf("%s %d\n", nameInfo[i].name, nameInfo[i].cnt);
-*/
+	fseek(inFp, 0, SEEK_SET);
+
+	NameData nameInfo[dataCnt];
+
+	for(i = 0; i< dataCnt; i++){
+		fgets(tmpIn, sizeof(tmpIn) , inFp);
+		sprintf(tmpClassName, "%s", strtok(tmpIn, " "));
+
+		nameInfo[i].name = (char *) malloc(sizeof(char) * strlen(tmpClassName) + 1);
+		strcpy(nameInfo[i].name, tmpClassName);
+
+		nameInfo[i].cnt = atoi(strtok(NULL, " "));
+	}
+
+	printf("Success, get datas from input file.\n");
+
+	qsort( nameInfo, dataCnt, sizeof(NameData), comp);
+
+	printf("Success, do quick sort.\n");
+
+	for(i = 0; i< dataCnt; i++){
 		sprintf(tmpOut, "%s %d\n", nameInfo[i].name, nameInfo[i].cnt);
-
-		printf("value = %s", tmpOut);
-		/* ファイルにデータを書き込み */
-		//fwrite(tmpOut, sizeof(char) * 255, 1, outFp);
 		fputs(tmpOut, outFp);
 	}
 
-	//fclose(inFp);
-	fclose(outFp);
+	printf("Success, write the sorted data to output file.\n");
+
+	for(i = 0; i < dataCnt; i++){
+		free(nameInfo[i].name);
+	}
+
+	closePointers(inFp, outFp);
+
+	printf("Success, close input and output file pointers.\n");
 }
 
+// Caution: Parameter pointer goes to end.
+int getRecordCntFromFile(FILE *targetFp){
+	char tmp[255] = {"\0"};
+	int cnt = 0;
+
+	while(fgets(tmp, sizeof(tmp) , targetFp) != NULL) {
+		cnt++;
+	}
+
+	return cnt;
+}
+
+int comp( const void *c1, const void *c2){
+	NameData test1 = *(NameData *)c1;
+	NameData test2 = *(NameData *)c2;
+
+	if(test1.cnt != test2.cnt){
+	//if change with cnt1 - cnt2, order will be ascending.
+		return test2.cnt - test1.cnt;
+
+	// when both cnt are same, compare both class name.
+	// In this case, order becomes ascending.
+	}else{
+		return strcmp(test1.name, test2.name);
+	}
+}
+/*
 int comp( const void *c1, const void *c2){
 	NameData test1 = *(NameData *)c1;
 	NameData test2 = *(NameData *)c2;
@@ -86,19 +123,9 @@ int comp( const void *c1, const void *c2){
 	//if change with cnt1 - cnt2, order will be ascending.
 	return cnt2 - cnt1;
 }
-
-/*
-	if((fp = fopen("sampleFile.txt", "r")) == NULL){
-			printf("file open error\n");
-			return 0;
-	}
-
-	while((tmpChar = fgetc(fp)) != EOF ) {
-		printf( "%c" , tmpChar);
-	}
 */
 
-/*
-	fclose(fp);
-*/
-
+void closePointers(FILE *fp1, FILE *fp2){
+	fclose(fp1);
+	fclose(fp2);
+}
