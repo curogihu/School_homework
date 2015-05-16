9dd333ce39ecb3e3e17c218feeb3bfb11d56937e
curogihu <curoas@yahoo.co.jp>
2015-05-15 23:01:49 -0700

    一部書き換え

--- classProject/mainFunction.c
+++ classProject/mainFunction.c
@@ -7,48 +7,67 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>		//atoi
+#include <stdlib.h>
+
+#define INPUTFILE "inputData.txt"
+#define OUTPUTFILE "outputData.txt"
+#define DEBUG 1
 
 typedef struct{
   char *name;
   int cnt;
-
 }NameData;
 
-int comp( const void *c1, const void *c2);
+FILE *sortDataByCnt(FILE *inFp);
 int getRecordCntFromFile(FILE *targetFp);
+int comp( const void *c1, const void *c2);
 void closePointers(FILE *fp1, FILE *fp2);
 
 int main(){
-
 	FILE *inFp, *outFp;
+
+	inFp = fopen(INPUTFILE, "r");
+
+	if((outFp = sortDataByCnt(inFp)) == NULL){
+		printf("Error, program couldn't sort data");
+	}
+
+}
+
+/*
+ * When executing this function, input file pointer is closed regardless of consequence.
+ *
+ * If an error occurs, return NULL.
+ * If no error occurs, return output File pointer.
+ */
+FILE *sortDataByCnt(FILE *inFp){
+	FILE *outFp;
 	char tmpIn[255] = {"\0"};
 	char tmpOut[255] = {"\0"};
 	char tmpClassName[255] = {"\0"};
 	int i;
 	int dataCnt;
 
-	if((inFp = fopen("inputData2.txt", "r")) == NULL){
-		printf("Input file open error\n");
-		return 0;
-	}
-
-	//if((outFp = fopen("./output.txt", "w")) == NULL){
-	if((outFp = fopen("outputData.txt", "w")) == NULL){
+	if((outFp = fopen(OUTPUTFILE, "w")) == NULL){
 		printf("Output file open error\n");
-		return 0;
+		return NULL;
 	}
 
+#ifdef DEBUG
 	printf("Success, two files are opened.\n");
+#endif
 
 	dataCnt = getRecordCntFromFile(inFp);
 
 	if(dataCnt <= 0){
 		printf("Program is finished because any data aren't found.\n");
 		closePointers(inFp, outFp);
+		return NULL;
 	}
 
+#ifdef DEBUG
 	printf("Success, get records count.\n");
+#endif
 
 	fseek(inFp, 0, SEEK_SET);
 
@@ -64,29 +83,40 @@ int main(){
 		nameInfo[i].cnt = atoi(strtok(NULL, " "));
 	}
 
+#ifdef DEBUG
 	printf("Success, get datas from input file.\n");
+#endif
 
 	qsort( nameInfo, dataCnt, sizeof(NameData), comp);
 
+#ifdef DEBUG
 	printf("Success, do quick sort.\n");
+#endif
 
 	for(i = 0; i< dataCnt; i++){
 		sprintf(tmpOut, "%s %d\n", nameInfo[i].name, nameInfo[i].cnt);
 		fputs(tmpOut, outFp);
 	}
 
+#ifdef DEBUG
 	printf("Success, write the sorted data to output file.\n");
+#endif
 
 	for(i = 0; i < dataCnt; i++){
 		free(nameInfo[i].name);
 	}
 
-	closePointers(inFp, outFp);
+	//closePointers(inFp, outFp);
+	fseek(outFp, 0, SEEK_SET);
 
+#ifdef DEBUG
 	printf("Success, close input and output file pointers.\n");
+#endif
+
+	return outFp;
 }
 
-// Caution: Parameter pointer goes to end.
+// Caution: Parameter pointer will be termination after executing.
 int getRecordCntFromFile(FILE *targetFp){
 	char tmp[255] = {"\0"};
 	int cnt = 0;
@@ -98,32 +128,18 @@ int getRecordCntFromFile(FILE *targetFp){
 	return cnt;
 }
 
-int comp( const void *c1, const void *c2){
+//Order by cnt desc, name asc
+int comp(const void *c1, const void *c2){
 	NameData test1 = *(NameData *)c1;
 	NameData test2 = *(NameData *)c2;
 
 	if(test1.cnt != test2.cnt){
-	//if change with cnt1 - cnt2, order will be ascending.
 		return test2.cnt - test1.cnt;
 
-	// when both cnt are same, compare both class name.
-	// In this case, order becomes ascending.
 	}else{
 		return strcmp(test1.name, test2.name);
 	}
 }
-/*
-int comp( const void *c1, const void *c2){
-	NameData test1 = *(NameData *)c1;
-	NameData test2 = *(NameData *)c2;
-
-	int cnt1 = test1.cnt;
-	int cnt2 = test2.cnt;
-
-	//if change with cnt1 - cnt2, order will be ascending.
-	return cnt2 - cnt1;
-}
-*/
 
 void closePointers(FILE *fp1, FILE *fp2){
 	fclose(fp1);
